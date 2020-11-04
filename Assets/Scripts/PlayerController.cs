719218568b8fb86a8235e08217bfccfc96dd6452
Clement Jaccarino <clementjaccarino@gmail.com>
2020-11-04 21:41:06 +0100

    Added player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerController : MonoBehaviour {
+    [SerializeField] private CharacterController2D controller2D;
+    [SerializeField] private float speed = 100f;
+
+    private Rigidbody2D playerRigidbody;
+    private float playerDirection;
+    private bool jump;
+
+    private void Update() {
+        playerDirection = Input.GetAxisRaw("Horizontal") * speed;
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            jump = true;
+        }
+    }
+
+    private void FixedUpdate() {
+        controller2D.Move(playerDirection * Time.deltaTime, jump);
+        jump = false;
+    }
+
+}
