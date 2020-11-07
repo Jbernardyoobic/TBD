1cee7efd749d1ba3ed087f4403c1e8f7797d5584
Clement Jaccarino <clementjaccarino@gmail.com>
2020-11-07 21:29:58 +0100

    Change the way level are generated and designed
    We load a Tilemap prefab that is created inside Unity rather than from a PNG.

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -1,9 +1,9 @@
-﻿using UnityEngine;
+﻿using System.Linq.Expressions;
+using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour {
 
-    public Texture2D[] map;
-    public ColorToPrefab[] colorMappings;
+    public GameObject[] levels;
 
     private void Update() {
         if (Input.GetKeyDown(KeyCode.Keypad1)) {
@@ -27,27 +27,7 @@ public class LevelGenerator : MonoBehaviour {
 
     void GenerateLevel(int mapIndex) {
         ClearLevel();
-        for (int x = 0; x < map[mapIndex].width; x++) {
-            for (int y = 0; y < map[mapIndex].height; y++) {
-                GenerateTile(x, y, mapIndex);
-            }
-        }
 
-    }
-
-    void GenerateTile(int x, int y, int mapIndex) {
-        Color pixelColor = map[mapIndex].GetPixel(x, y);
-
-        if (pixelColor.a == 0) {
-            return;
-        }
-
-        foreach (ColorToPrefab colorMapping in colorMappings) {
-            if (colorMapping.color.Equals(pixelColor)) {
-                Vector2 position = new Vector2(x, y);
-                Instantiate(colorMapping.prefab, position, Quaternion.identity, transform);
-            }
-        }
-
+        Instantiate(levels[mapIndex], gameObject.transform.position, Quaternion.identity, transform);
     }
 }
