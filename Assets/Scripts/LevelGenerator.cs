6b1297a8350df15a39cfeae5c21b62e44313834d
Jbernardyoobic <jbernard@yoobic.com>
2020-11-08 11:28:24 +0100

    add onTrigger Script
    Does nothing for now

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -5,7 +5,13 @@ public class LevelGenerator : MonoBehaviour {
 
     public GameObject[] levels;
 
+    private int currentLevel = -1;
+
     private void Update() {
+        if (currentLevel < 0) {
+            GenerateLevel(0);
+        }
+
         if (Input.GetKeyDown(KeyCode.Keypad1)) {
             GenerateLevel(0);
         }
@@ -27,7 +33,7 @@ public class LevelGenerator : MonoBehaviour {
 
     void GenerateLevel(int mapIndex) {
         ClearLevel();
-
+        currentLevel = mapIndex;
         Instantiate(levels[mapIndex], gameObject.transform.position, Quaternion.identity, transform);
     }
 }
