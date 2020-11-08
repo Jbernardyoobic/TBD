5966fd9de96b5f29ad79793ffb57e9c5ad049428
Clement Jaccarino <clementjaccarino@gmail.com>
2020-11-08 19:09:18 +0100

    First implemention of timer

---
+++ Assets/Scripts/TimerHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class TimerHandler : MonoBehaviour {
+
+    [SerializeField] private TextMeshProUGUI timer;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        timer.SetText("Time : " + System.Math.Round(Time.time, 2) + " s");
+    }
+}
