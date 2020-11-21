ed40651a759f1cf338eb54c2e8b0e3e77a246cab
Jbernardyoobic <jbernard@yoobic.com>
2020-11-21 15:21:30 +0100

    Add Menu play quit

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+    
+    public Button play;
+    public Button quit;
+
+    void Start() {
+        play.onClick.AddListener(onPlayClick);
+        quit.onClick.AddListener(onQuitClick);
+    }
+
+    void onPlayClick() {
+        SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+    }
+
+    void onQuitClick() {
+        Application.Quit();
+    }
+}
