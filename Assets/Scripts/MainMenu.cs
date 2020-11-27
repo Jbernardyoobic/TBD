01f97e1521e50fa46f8b63528bd86b2b53b20df2
Jbernardyoobic <jbernard@yoobic.com>
2020-11-27 14:52:20 +0100

    add options menu (empty)

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,14 +8,39 @@ public class MainMenu : MonoBehaviour {
     
     public Button play;
     public Button quit;
+    public Button quitOptions;
+    public Button options;
+
+    private GameObject optionsObject;
+    private GameObject[] menuObjects;
 
     void Start() {
         play.onClick.AddListener(onPlayClick);
         quit.onClick.AddListener(onQuitClick);
+        options.onClick.AddListener(onOptionsClick);
+        quitOptions.onClick.AddListener(onQuitOptionsClick);
+
+        menuObjects = GameObject.FindGameObjectsWithTag("MainMenu");
+        optionsObject = GameObject.FindGameObjectWithTag("Options");      
+        optionsObject.SetActive(false);
     }
 
     void onPlayClick() {
         SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+    }
+
+    void onOptionsClick() {
+        foreach (GameObject item in menuObjects) {   
+            item.SetActive(false);
+        }
+        optionsObject.SetActive(true);
+    }
+
+    void onQuitOptionsClick() {
+        optionsObject.SetActive(false);
+        foreach (GameObject item in menuObjects) {   
+            item.SetActive(true);
+        }
     }
 
     void onQuitClick() {
