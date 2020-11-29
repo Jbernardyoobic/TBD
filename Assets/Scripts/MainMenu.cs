1285f95f2c824552c0cb80f741625b8eb8922700
Clement Jaccarino <clementjaccarino@gmail.com>
2020-11-29 11:18:41 +0100

    Change menu script

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,49 +1,35 @@
-﻿using System.Collections;
+﻿using System.Diagnostics.Tracing;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
-    
-    public Button play;
-    public Button quit;
-    public Button quitOptions;
-    public Button options;
 
-    private GameObject optionsObject;
-    private GameObject[] menuObjects;
+    public Canvas mainMenu;
+    public Canvas optionsMenu;
 
     void Start() {
-        play.onClick.AddListener(onPlayClick);
-        quit.onClick.AddListener(onQuitClick);
-        options.onClick.AddListener(onOptionsClick);
-        quitOptions.onClick.AddListener(onQuitOptionsClick);
-
-        menuObjects = GameObject.FindGameObjectsWithTag("MainMenu");
-        optionsObject = GameObject.FindGameObjectWithTag("Options");      
-        optionsObject.SetActive(false);
+        mainMenu.enabled = true;
+        optionsMenu.enabled = false;
     }
 
-    void onPlayClick() {
+    public void onPlayClick() {
         SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
     }
 
-    void onOptionsClick() {
-        foreach (GameObject item in menuObjects) {   
-            item.SetActive(false);
-        }
-        optionsObject.SetActive(true);
+    public void onOptionsClick() {
+        mainMenu.enabled = false;
+        optionsMenu.enabled = true;
     }
 
-    void onQuitOptionsClick() {
-        optionsObject.SetActive(false);
-        foreach (GameObject item in menuObjects) {   
-            item.SetActive(true);
-        }
+    public void onQuitOptionsClick() {
+        optionsMenu.enabled = false;
+        mainMenu.enabled = true;
     }
 
-    void onQuitClick() {
+    public void onQuitClick() {
         Application.Quit();
     }
 }
